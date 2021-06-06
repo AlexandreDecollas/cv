5efddcc9265c519f00de494d7a2bf48703bcb5b4
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2021-06-06 22:59:09 +0200

    ssr works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
-  // { path: "contact", component: ContactComponent },
+  { path: "**", redirectTo: '' },
 ];
 
 @NgModule({
