220d7a2183bbe2ac47524b670df95434e24883a7
Alexandre Decollas <ade@moonda.com>
2021-06-08 11:09:05 +0200

    update to angular 12

--- src/app/sections/content/journey/experience/school-experience/school-experience.component.spec.ts
+++ src/app/sections/content/journey/experience/school-experience/school-experience.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SchoolExperienceComponent } from './school-experience.component';
 
@@ -6,7 +6,7 @@ describe('SchoolExperienceComponent', () => {
   let component: SchoolExperienceComponent;
   let fixture: ComponentFixture<SchoolExperienceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SchoolExperienceComponent ]
     })
